7d5d627961058440f867d720bbabec3fca507ab2
Eric Zhang <ccme66@gmail.com>
2021-10-03 22:03:33 +0800

    Upload New Testing Arcade Drive(Talon)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Talon;
 
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -31,14 +32,22 @@ public class Robot extends TimedRobot {
   private final Talon R2 = new Talon(2);
   private final Talon L1 = new Talon(3);
   private final Talon L2 = new Talon(4);*/
-  private final SpeedController S_R1 = new Talon(1);
-  private final SpeedController S_R2 = new Talon(2);
-  private final SpeedController S_L1 = new Talon(3);
-  private final SpeedController S_L2 = new Talon(4);
+  /*Talon S_R1 = new Talon(1);
+  Talon S_R2 = new Talon(2);
+  Talon S_L1 = new Talon(3);
+  Talon S_L2 = new Talon(4);
   private final SpeedControllerGroup M_L = new SpeedControllerGroup(S_L1, S_L2);
   private final SpeedControllerGroup M_R = new SpeedControllerGroup(S_R1, S_R2);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(M_L, M_R);
-  
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(M_L, M_R);*/
+  Talon m_frontLeft = new Talon(3);
+  Talon m_rearLeft = new Talon(4);
+  SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
+
+  Talon m_frontRight = new Talon(1);
+  Talon m_rearRight = new Talon(2);
+  SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
+
+  DifferentialDrive m_robotDrive = new DifferentialDrive(m_left, m_right);
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
