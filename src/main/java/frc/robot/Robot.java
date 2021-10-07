c70b8c83cf4422595377234d25e5c32f9a2928ff
Eric Zhang <ccme66@gmail.com>
2021-10-07 20:32:14 +0800

    Fix #1

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,13 +8,14 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.SpeedController;
+//import edu.wpi.first.wpilibj.SpeedController;
+//import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.Talon;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 
 /**
@@ -25,28 +26,16 @@ import edu.wpi.first.wpilibj.Talon;
  * directory.
  */
 public class Robot extends TimedRobot {
-  //private final DifferentialDrive m_robotDrive = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+//private final DifferentialDrive m_robotDrive = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
   private final Joystick m_stick = new Joystick(0);
   private final Timer m_timer = new Timer();
-  /*private final Talon R1 = new Talon(1);
-  private final Talon R2 = new Talon(2);
-  private final Talon L1 = new Talon(3);
-  private final Talon L2 = new Talon(4);*/
-  /*Talon S_R1 = new Talon(1);
-  Talon S_R2 = new Talon(2);
-  Talon S_L1 = new Talon(3);
-  Talon S_L2 = new Talon(4);
-  private final SpeedControllerGroup M_L = new SpeedControllerGroup(S_L1, S_L2);
-  private final SpeedControllerGroup M_R = new SpeedControllerGroup(S_R1, S_R2);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(M_L, M_R);*/
-  Talon m_frontLeft = new Talon(3);
-  Talon m_rearLeft = new Talon(4);
-  SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
 
-  Talon m_frontRight = new Talon(1);
-  Talon m_rearRight = new Talon(2);
+  private final SpeedController m_frontRight = new WPI_TalonSRX(1);
+  private final SpeedController m_rearRight = new WPI_TalonSRX(2);
+  private final SpeedController m_frontLeft = new WPI_TalonSRX(3);
+  private final SpeedController m_RearLeft = new WPI_TalonSRX(4);
+  SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_RearLeft);
   SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
-
   DifferentialDrive m_robotDrive = new DifferentialDrive(m_left, m_right);
   /**
    * This function is run when the robot is first started up and should be
